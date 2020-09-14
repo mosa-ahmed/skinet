32fbeecb546d66d959f296c565940e039d8301ad
unknown <mousa.ahmed.mustafa@gmail.com>
2020-09-14 10:09:35 +0200

    API - Validation

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -98,6 +98,12 @@ namespace API.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
+                //we need to get .Result.Value because this is an async method
+            if (this.CheckEmailExistsAsync(registerDto.Email).Result.Value)
+            {
+                return new BadRequestObjectResult(new ApiValidationErrorResponse{Errors = new []{"Email address is in use"}});
+            }
+
             var user = new AppUser
             {
                 DisplayName = registerDto.DisplayName,
